ddb46bd7be1a23259f50bab94510f4aa6c880b21
Juan Luis <juannluissbarriossgarciaa@gmail.com>
2024-08-07 23:02:22 +0200

    modifique el index.html

---
+++ script.js
@@ -0,0 +1,19 @@
+let datoString = "Mensaje"
+let datoNumber = 2024
+let datoBoolean = true
+
+let datoUndefined = undefined
+let datoNull = null
+
+let datoLista = [
+    "Cereal",
+    "Arroz",
+    "Pasta",
+    "Harina",
+]
+
+
+
+
+console.log(datoLista)
+
